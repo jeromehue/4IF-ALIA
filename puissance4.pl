098dee396ecf417b77e467432b388fc955ca88e8
YohanMeyer <yohan.meyer@outlook.fr>
2020-10-26 10:50:24 +0100

    ajout du jeu

--- puissance4.pl
+++ puissance4.pl
@@ -17,13 +17,13 @@
 
 
 % Create board of 6 lines and 7 columns.
-create(board([  ['-','-','-','-','-','O'],
-	            ['-','-','-','-','X','O'],
-	            ['-','-','-','-','-','X'],
-	            ['-','-','-','-','-','0'],
-	            ['-','-','-','-','-','X'],
+create([  ['-','-','-','-','-','-'],
 	            ['-','-','-','-','-','-'],
-	            ['-','-','-','-','-','-']   ])).
+	            ['-','-','-','-','-','-'],
+	            ['-','-','-','-','-','-'],
+	            ['-','-','-','-','-','-'],
+	            ['-','-','-','-','-','-'],
+	            ['-','-','-','-','-','-']   ]).
 
 
 
@@ -31,7 +31,7 @@ create(board([  ['-','-','-','-','-','O'],
 
 
 
-%show(X) shows boarddd X
+%show(X) shows board X
 show(board(X)):- write('  A B C D E F G'), nl,
 		 iShow(X,6).
 
@@ -52,15 +52,47 @@ iShowLine([[X|X2]|XS],[X2|XS2]):- write(X), write(' '),
 
 
 
-% Play a move
-play(Player) :- write('New turn for player : '), writeln(Player),
-                A is random(7),
-                write('Playing in the column n° : '), writeln(A).
+% Game is over, we cut to stop the search, and display the winner.
+%play(_, Board) :- gameover(Winner), !, write('Game is Over. Winner: '), writeln(Winner), show(Board).
 
-% Jouer un coup. 
+% The game is not over, we play the next turn
+play(Player, Board) :- write('New turn for:'), writeln(Player),
+	show(board(Board)),
+	%ia(Board, Move, Player),
+	read(Move),
+	writeln(Move),
+	NewBoard=Board,
+	writeln("BOARD OK"),
+	playMove(Board, NewBoard, Move, Player, 0),
+	writeln("MOVE OK"),
+	changePlayer(Player,NextPlayer),
+	writeln("CHANGE OK"),
+	play(NextPlayer, NewBoard).
+	
+colonneN([T|_],1,T).
+colonneN([_|Q],C,X) :-
+    C1 is C-1,
+    colonneN(Q,C1,X).
+		
+playMove(Board, NewBoard, Move, Player, Index) :-
+	Index<7, colonneN(Board, Move, Col), nth0(Index, Col, '-'), colonneN(NewBoard, Move, NewCol), nth0(Index, NewCol, Player).
+	%;Index<7,NewIndex is Index+1, playMove(Board, NewBoard, Move, Player, NewIndex).
+	
+
+changePlayer('X', 'O').
+changePlayer('O', 'X').
+
+
+winnerColonne([Y|B],X):-Y==X,B=[X,X,X|_];winnerColonne(B,X).
+
+
+extract(ColNumber, Matrix, Column) :- maplist(nth0(ColNumber), Matrix, Column).
+
+winnerLigne1(Board,X,Num):-extract(Num,Board,L1),winnerColonne(L1,X).
+winnerLigne2(Board,X):-winnerLigne1(Board,X,0);winnerLigne1(Board,X,1);winnerLigne1(Board,X,2);winnerLigne1(Board,X,3);winnerLigne1(Board,X,4);winnerLigne1(Board,X,5);winnerLigne1(Board,X,6);winnerLigne1(Board,X,7).
 
 
 %Initialisation
-init :- create(X), show(X).
-
-
+%init :- create(B), play('X', B).
+%create(B), colonneN(B,1,C), nth0(1, C, '-'),B=NewBoard,colonneN(NewBoard, 1, NewCol), nth0(0, NewCol, 'a'), show(board(NewBoard)), show(board(B)).
+% not working ! But working in morpion ! Why N
